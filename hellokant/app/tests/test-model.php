be54aab4adcb74ad42ae275f0a042995992e1f7e
Damien <damien@LAPTOP-OE4I5EUT.localdomain>
2023-01-15 17:49:56 +0100

    finders presque fini mais fonctionnelles

--- hellokant/app/tests/test-model.php
+++ hellokant/app/tests/test-model.php
@@ -11,10 +11,18 @@ require_once '../models/Article.php';
 echo "TEST..............";
 
 
-$article = new hellokant\models\Article(array('name' => "Article1", 'desc' => "Description", 'price' => 20), parse_ini_file("../../src/connection/conf.ini"));
+$article = new hellokant\models\Article(array('name' => "Article1", 'description' => "Description", 'price' => 20), parse_ini_file("../../src/connection/conf.ini"));
 $article->name = "Article1";
 $article->desc = "Description";
 $article->price = 20;
+
+// $article1 = new hellokant\models\Article(array('name' => "Article2", 'desc' => "Description: Article2", 'price' => 200), parse_ini_file("../../src/connection/conf.ini"));
+// var_dump($article1->attributes);
+// $article1->delete();
+$test = new hellokant\models\Article(array('name' => "TESSSST", 'description' => "Description: TESSSSSST", 'price' => 20000), parse_ini_file("../../src/connection/conf.ini"));
+// $test->insert();
+// var_dump($test);
+// $test->delete();
 // var_dump($article);
 
 
@@ -22,7 +30,7 @@ $article->price = 20;
 // echo ("Description : " . $article->__get('desc'));
 // echo ("Prix : " . $article->__get('price'));
 
-//$query = new hellokant\query\Query('article', parse_ini_file("../../src/connection/conf.ini"));
+// $query = new hellokant\query\Query('article', parse_ini_file("../../src/connection/conf.ini"));
 
 //Delete
 //$query->where('id', '=', 3);
@@ -30,26 +38,36 @@ $article->price = 20;
 
 // Insert
 // $query->setField('name');
-// $query->insert('\'David\'');
+// $query->insert('\'TESSSSST\'');
 // $query->setField('description');
-// $query->insert('\'Petite description\'');
+// $query->insert('\'Petite description TEST\'');
 // $query->setField('price');
-// $query->insert(20);
+// $query->insert(20000000);
 
-// $id = $query
-//     ->insert(array('name' => "Article10", 'description' => "Description", 'price' => 50));
+
+// $id = $query->insert(array('name' => "Article10TEST", 'description' => "DescriptionTEST", 'price' => 50000));
 
 // $article = Article::all();
-// // foreach ($article as $art) {
-// //     printf("\nName : " . $art->__get('name'));
-// // }
+// foreach ($article as $art) {
+//     printf("\nName : " . $art->__get('name'));
+// }
 // var_dump($article);
+
+// $article = Article::find(11);
+// echo ("FIND!!!!!!: ");
+// var_dump($article[0]);
+
+
+// $article = Article::one(8);
+// var_dump($article);
+
+
+$article = Article::find(['name', 'price'], 21);
+var_dump($article);
 
 // $article = Article::find();
 // var_dump($article);
 
-$article = Article::find([8, 9]);
-var_dump($article);
 
 //$article = Article::find(['name', 'price'], 7);
 //var_dump($article);
